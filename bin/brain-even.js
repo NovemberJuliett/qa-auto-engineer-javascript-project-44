95a34bd8e341c5e2741da1a1f7df11950a3e4b60
Iuliia Izmailova <izmaylovajulia@yandex.ru>
2024-12-30 22:45:02 +0200

    dd new logic

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,18 +4,18 @@ console.log('Answer "yes" if the number is even, otherwise answer "no". ');
 const min = 0;
 const max = 100;
 
-
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+let random_number = getRandomNumber(min, max)
+console.log("Question: " + random_number)
 
-let users_number = readlineSync.question("Your answer: ");
-if (random_number && users_number % 2 === 0){
+let users_answer = readlineSync.question("Your answer: ");
+if (random_number % 2 === 0 && users_answer === "yes") {
     console.log("Correct!")
 }
-
-console.log("Question: " )
-const random_number = getRandomNumber(min, max)
-
-isEven()
+else if (random_number % 2 !== 0 && users_answer === "no"){
+    console.log("Correct!")
+}
+else(console.log("'${users_answer} is wrong answer ;(. Correct answer was 'no'. Let's try again, Bill!"))
