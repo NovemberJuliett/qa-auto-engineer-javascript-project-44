c0c59caa55192cb5b2a6434894b2fbaf19afd422
Iuliia Izmailova <izmaylovajulia@yandex.ru>
2025-01-08 13:46:32 +0200

    Fixed minor bugs

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
-import greetUser from '../src/cli.js';
+import greetUser from "../src/cli.js";
 
-console.log('Answer "yes" if the number is even, otherwise answer "no". ');
 const min = 0;
 const max = 100;
+const userName = greetUser()
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -12,9 +12,12 @@ function getRandomNumber(min, max) {
 let random_number = getRandomNumber(min, max)
 console.log("Question: " + random_number)
 
+console.log('Answer "yes" if the number is even, otherwise answer "no". ');
+
 let users_answer = readlineSync.question("Your answer: ");
 let positive_answer = "yes"
 let negative_answer = "no"
+
 
 if (random_number % 2 === 0 && users_answer === positive_answer) {
     console.log("Correct!")
@@ -25,9 +28,12 @@ if (random_number % 2 !== 0 && users_answer === negative_answer){
 else if(random_number % 2 === 0 && users_answer === negative_answer){
     (console.log("'" + negative_answer + "'", "is wrong answer ;(. Correct answer was",
         "'" + positive_answer + "'"))
+    console.log("Let's try again,", userName + "!")
 }
 
-else(console.log("'" + positive_answer + "'", "is wrong answer ;(. Correct answer was",
+if(random_number % 2 !== 0 && users_answer === positive_answer){
+    (console.log("'" + positive_answer + "'", "is wrong answer ;(. Correct answer was",
         "'" + negative_answer + "'"))
+    console.log("Let's try again,", userName + "!")
+}
 
-greetUser()
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,13 @@
-import readlineSync from 'readline-sync';
+import readlineSync from "readline-sync";
+
 
 function greetUser() {
-    let userName = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + userName + '!');
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+    return name;
 }
 
 export default greetUser;
+
+
