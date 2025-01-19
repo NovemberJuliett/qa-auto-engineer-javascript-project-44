427de9bcd22d6ad1c43eb47fde88fab633bf04bf
Iuliia Izmailova <izmaylovajulia@yandex.ru>
2025-01-19 18:56:10 +0200

    Add new logic

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import readlineSync from "readline-sync";
 const userName = greetUser()
 let usersAnswer = readlineSync.question("Your answer: ");
 
-if (usersAnswer === correctAnswer) {
-    console.log("Correct!");
-    ++counter
-} else {
-    console.log(`'${usersAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-    console.log("Let's try again,", userName + "!")
-    break
+
+function checkAnswer(usersAnswer, correctAnswer) {
+    let counter = 0;
+    while (counter < 3) {
+
+        if (usersAnswer === correctAnswer) {
+            console.log("Correct!");
+            ++counter
+        } else {
+            console.log(`'${usersAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+            console.log("Let's try again,", userName + "!")
+            break
+        }
+    }
+    console.log("Congratulations", userName + "!");
 }
-}
-console.log("Congratulations", userName + "!");
